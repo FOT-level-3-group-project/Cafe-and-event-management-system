47c93f9906eccc4c219b8ee3da046cf9714fc877
Ashen694 <akavindu288@gmail.com>
2024-02-28 14:58:54 +0530

    firstCommit
    ashen

---
+++ Spring Boot - Back End/src/main/java/com/kingsman/Kingsman/KingsmanApplication.java
@@ -0,0 +1,14 @@
+package com.kingsman.Kingsman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingsmanApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Let's begin our project. Give your best effort to this project.");
+
+	}
+
+}
