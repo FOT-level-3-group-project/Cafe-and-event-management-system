1f2b3c2f54a1dbacc3043b51d69f17ad348e02c4
Thisara Bandara <thisarabandara2@gmail.com>
2024-03-29 16:25:08 +0530

    Create the header section

--- ReactFrontEnd/kingsman/src/App.jsx
+++ ReactFrontEnd/kingsman/src/App.jsx
@@ -4,10 +4,12 @@ import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import About from './pages/About'
 import Login from './pages/Login'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/dashboard" element={<Dashboard/>} />
