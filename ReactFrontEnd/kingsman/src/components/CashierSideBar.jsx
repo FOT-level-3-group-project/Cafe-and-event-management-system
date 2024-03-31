4031fc2ec06dcc521d8b1d06f63f895c2907e9db
Thisara Bandara <thisarabandara2@gmail.com>
2024-04-01 03:13:36 +0530

    Create Cashier and Manager side bar

---
+++ ReactFrontEnd/kingsman/src/components/CashierSideBar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import { Link, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+import { Sidebar } from "flowbite-react";
+import { HiArrowSmRight, HiUser } from "react-icons/hi";
+
+export default function CashierSideBar() {
+    const location = useLocation();
+    const [tab, setTab] = useState('');
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const tabFromUrl = urlParams.get('tab');
+        if (tabFromUrl) {
+            setTab(tabFromUrl);
+        }
+    }, [location.search]);
+  return (
+    <Sidebar className='w-full md:w-56'>
+            <Sidebar.Items>
+                <Sidebar.ItemGroup>
+                    <Link to='/cashier?tab=attendance'>
+                        <Sidebar.Item active={tab === 'attendance'} icon={HiUser} >
+                            Attendance
+                        </Sidebar.Item>
+                    </Link>
+                    <Link to='/cashier?tab=profile'>
+                        <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"User"} labelColor='dark'>
+                            Profile
+                        </Sidebar.Item>
+                    </Link>
+                    <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' >
+                        Log Out
+                    </Sidebar.Item>
+
+                </Sidebar.ItemGroup>
+            </Sidebar.Items>
+        </Sidebar>
+  )
+}
---
+++ ReactFrontEnd/kingsman/src/pages/cashier/CashierProfile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CashierProfile() {
+  return (
+    <div>
+      Cash Profile
+    </div>
+  )
+}
