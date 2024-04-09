4c5fd16e684eee5a302cfc10c460fe98bdec5104
Chathumina Dilshan <chathuminadilshan14@gmail.com>
2024-04-09 15:43:08 +0530

    Create waiter sidebar

--- ReactFrontEnd/kingsman/src/pages/waiter/Waiter.jsx
+++ ReactFrontEnd/kingsman/src/pages/waiter/Waiter.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import WaiterSideBar from '../../components/WaiterSideBar'
+import TakeOrder from './order/TakeOrder';
 
 export default function Waiter() {
+
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+
+  useEffect(() => {
+      const urlParams = new URLSearchParams(location.search);
+      const tabFromUrl = urlParams.get('tab');
+      if (tabFromUrl) {
+          setTab(tabFromUrl);
+      }
+  }, [location.search]);
+
   return (
+
     <div>
-      Waiter
+        <div className='min-h-screen flex flex-col md:flex-row'>
+            <div className='md:w-56'>
+                {/* sidebar */}
+                <WaiterSideBar/>
+            </div>
+
+                {/* Inventory */}
+                {tab === 'take-order' && <TakeOrder/>}
+                {/* profile */}
+                {/* {tab === 'profile' && <ManagerProfile/>} */}
+        </div>
     </div>
+
   )
 }
+
---
+++ ReactFrontEnd/kingsman/src/pages/waiter/order/TakeOrder.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function TakeOrder() {
+
+
+  return (
+    <div>
+      waiter
+    </div>
+  )
+}
