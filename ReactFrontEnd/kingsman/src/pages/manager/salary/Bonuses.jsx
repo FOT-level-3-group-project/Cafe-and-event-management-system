b793c7fe5bf2149ca9e947b70e83563b038085de
Ashen694 <akavindu288@gmail.com>
2024-04-12 13:52:10 +0530

    bonus changes

--- ReactFrontEnd/kingsman/src/pages/manager/salary/Bonuses.jsx
+++ ReactFrontEnd/kingsman/src/pages/manager/salary/Bonuses.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Table } from "flowbite-react";
 import { Button } from "flowbite-react";
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
 function Bonuses() {
   return (
@@ -19,6 +20,7 @@ function Bonuses() {
         <Table.HeadCell>Employee Name</Table.HeadCell>
         <Table.HeadCell>Bonus Type</Table.HeadCell>
         <Table.HeadCell>Bonus (R<span style={{ textTransform: 'lowercase' }}>s</span>.)</Table.HeadCell>
+        <Table.HeadCell>Action</Table.HeadCell>
 
         </Table.Head>
         <Table.Body className="divide-y">
@@ -27,17 +29,30 @@ function Bonuses() {
             <Table.Cell>W.A.T.I Bandara</Table.Cell>
             <Table.Cell>New Year Bonus</Table.Cell>
             <Table.Cell>2000</Table.Cell>
+            <Table.Cell className="flex">
+              <FaEdit className="text-blue-500 mr-2 hover:text-blue-700 cursor-pointer text-lg" />
+              <FaTrash className="text-red-500 hover:text-red-700 cursor-pointer text-lg" />
+            </Table.Cell>
           </Table.Row>
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell>W.G Madhushan</Table.Cell>
             <Table.Cell>Attendance Bonus</Table.Cell>
             <Table.Cell>2500</Table.Cell>
+            <Table.Cell className="flex">
+              <FaEdit className="text-blue-500 mr-2 hover:text-blue-700 cursor-pointer text-lg" />
+              <FaTrash className="text-red-500 hover:text-red-700 cursor-pointer text-lg" />
+            </Table.Cell>
           </Table.Row>
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell>C.D Senarathna</Table.Cell>
             <Table.Cell>Attendance Bonus</Table.Cell>
             <Table.Cell>2500</Table.Cell>
+            <Table.Cell className="flex">
+              <FaEdit className="text-blue-500 mr-2 hover:text-blue-700 cursor-pointer text-lg" />
+              <FaTrash className="text-red-500 hover:text-red-700 cursor-pointer text-lg" />
+            </Table.Cell>
           </Table.Row>
+ 
           
 
         </Table.Body>
@@ -50,6 +65,7 @@ function Bonuses() {
           <Table.HeadCell>Employee Name</Table.HeadCell>
           <Table.HeadCell>Diduction Type</Table.HeadCell>
           <Table.HeadCell>Diduction (R<span style={{ textTransform: 'lowercase' }}>s</span>.)</Table.HeadCell>
+          <Table.HeadCell>Action</Table.HeadCell>
         </Table.Head>
         <Table.Body className="divide-y">
           
@@ -57,16 +73,28 @@ function Bonuses() {
             <Table.Cell>W.A.T.I Bandara</Table.Cell>
             <Table.Cell>Advance</Table.Cell>
             <Table.Cell>2000</Table.Cell>
+            <Table.Cell className="flex">
+              <FaEdit className="text-blue-500 mr-2 hover:text-blue-700 cursor-pointer text-lg" />
+              <FaTrash className="text-red-500 hover:text-red-700 cursor-pointer text-lg" />
+            </Table.Cell>
           </Table.Row>
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell>W.G Madhushan</Table.Cell>
             <Table.Cell>Instrument</Table.Cell>
             <Table.Cell>2500</Table.Cell>
+            <Table.Cell className="flex">
+              <FaEdit className="text-blue-500 mr-2 hover:text-blue-700 cursor-pointer text-lg" />
+              <FaTrash className="text-red-500 hover:text-red-700 cursor-pointer text-lg" />
+            </Table.Cell>
           </Table.Row>
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell>C.D Senarathna</Table.Cell>
             <Table.Cell>Adance</Table.Cell>
             <Table.Cell>2500</Table.Cell>
+            <Table.Cell className="flex">
+              <FaEdit className="text-blue-500 mr-2 hover:text-blue-700 cursor-pointer text-lg" />
+              <FaTrash className="text-red-500 hover:text-red-700 cursor-pointer text-lg" />
+            </Table.Cell>
           </Table.Row>
 
         </Table.Body>
