cd709e0a74f45c9a874c01a10004cb36f0def851
Hiruni Dilmika <hirudilmih@gmail.com>
2024-04-25 22:41:42 +0530

    .

--- ReactFrontEnd/kingsman/src/pages/manager/Manager.jsx
+++ ReactFrontEnd/kingsman/src/pages/manager/Manager.jsx
@@ -10,6 +10,7 @@ import ViewAllEmployees from './manageEmployees/ViewAllEmployees';
 import AddEvent from './manageEvents/AddEvent';
 import ViewAllEvents from './manageEvents/ViewAllEvents';
 // import UpdateEvent from './manageEvents/UpdateEvent';
+import { MonthlyIncome } from './monthlyIncome/MonthlyIncome';
 
 
 export default function Manager() {
@@ -43,6 +44,8 @@ export default function Manager() {
                 {tab === 'view-all-events' && <ViewAllEvents/>}
                 {/* Update Event */}
                 {/* {tab === 'update-event' && <UpdateEvent/>} */}
+                {/* Monthly Income */}
+                {tab === 'monthly-income' && <MonthlyIncome/>}
 
         </div>
     )
