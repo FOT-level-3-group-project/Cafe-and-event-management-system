ccc1fa7e3e915d023b947553a0dd326e7efa3db9
Hiruni Dilmika <hirudilmih@gmail.com>
2024-04-26 10:58:32 +0530

    updated UIs

--- ReactFrontEnd/kingsman/src/pages/manager/manageEvents/ViewAllEvents.jsx
+++ ReactFrontEnd/kingsman/src/pages/manager/manageEvents/ViewAllEvents.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { Label, TextInput, Alert } from 'flowbite-react';
+import { Table } from "flowbite-react";
 // import UpdateEvent from './UpdateEvent';
 
 const ViewAllEvents = () => {
@@ -209,79 +210,85 @@ const ViewAllEvents = () => {
     }
     
     return (
-        <div className="container mx-auto px-4 py-8">
-            <div className="container mx-auto px-4 py-8 flex justify-between items-center ">
-                <h1 className="text-3xl font-bold mb-4 ">Manage Events</h1>
+    <div className="container mx-auto px-4 py-8">
+      <div className="container mx-auto px-4 py-8 flex justify-between items-center">
+        <h1 className="text-3xl font-bold mb-4">Manage Events</h1>
 
-                <div className="flex items-center">
-                    {/* Add search bar */}
-                    <div className='flex-grow px-4 py-2 border rounded-full dark:bg-gray-600 '>
-                        <input type="search" placeholder="Search Event..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} id="search"   className='flex-grow px-4 py-2 border-none outline-none focus:ring-0 dark:bg-gray-600 dark:text-white' />   
-                    </div>
-            
-                    {/* Add Event button */}
-                    <Link to="/manager?tab=add-event" className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 ml-2 rounded  px-4 ">Add Event</Link>
-                </div>
-            </div>
+        <div className="flex items-center">
+          {/* Search bar */}
+          <div className='flex-grow px-4 py-2 border rounded-full dark:bg-gray-600 '>
+            <input 
+              type="search" 
+              placeholder="Search Event..." 
+              value={searchQuery} 
+              onChange={(e) => setSearchQuery(e.target.value)} 
+              id="search"   
+              className='flex-grow px-4 py-2 border-none outline-none focus:ring-0 dark:bg-gray-600 dark:text-white' 
+            />   
+          </div>
 
-            <div className="relative overflow-x-auto">
-                <table className="w-full table-auto text-gray-700 dark:text-white-400 border-collapse bg-gray-50 ">
-                    <thead className="text-gray-700 text-sm uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-700">
-                        <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 dark:border-gray-700">
-                            <th className="px-4 py-2 text-center bg-gray-200">ID</th>
-                            <th className="px-4 py-2 text-center bg-gray-200">Name</th>
-                            <th className="px-4 py-2 text-center bg-gray-200">Date</th>
-                            <th className="px-4 py-2 text-center bg-gray-200">Starting Time</th>
-                            <th className="px-4 py-2 text-center bg-gray-200">Duration (h)</th>
-                            <th className="px-4 py-2 text-center bg-gray-200">Budget (Rs.)</th>
-                            <th className="px-4 py-2 text-center bg-gray-200">Ticket Price (Rs.)</th>
-                            <th className="px-4 py-2 text-center bg-gray-200">Quantity</th>
-                            <th className="px-4 py-2 text-center bg-gray-200 w-32">Entertainer</th>
-                            <th className="px-4 py-2 text-center bg-gray-200"> Description</th>
-                            <th className="px-4 py-2 text-center bg-gray-200" colSpan='3'>Actions</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        {events.filter(event => {
-                            // Filter events based on the search query
-                            if (!searchQuery) {
-                                return true; // Return all events if there's no search query
-                            } else {
-                                // Check if any of the event properties contain the search query
-                                return Object.values(event).some( value =>
-                                    value && value.toString().toLowerCase().includes(searchQuery.toLowerCase())
-                                );
-                            }
-                        })
-                        .map((event, index) => (
-                            <tr key={event.eventID} className={index % 2 === 0 ? "bg-gray-100 dark:bg-gray-600 dark:text-white" : "bg-gray-200 dark:bg-gray-700 dark:text-white"}>
-                                <td className="px-4 py-2">{event.eventID}</td>
-                                <td className="px-4 py-2">{event.eventName}</td>
-                                <td className="px-4 py-2">{event.eventDate}</td>
-                                <td className="px-4 py-2">{event.startTime}</td>
-                                <td className="px-4 py-2">{event.duration}</td>
-                                <td className="px-4 py-2">{event.budget}</td>
-                                <td className="px-4 py-2">{event.ticketPrice}</td>
-                                <td className="px-4 py-2">{event.ticketQuantity}</td>
-                                <td className="px-4 py-2">{event.entertainer}</td>
-                                <td className="px-4 py-2">{event.description}</td>
-                                <td className="px-6 py-4 text-right">
-                                   <button onClick={() => handleUpdate(event.eventID)} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Update</button>
-                                </td>
-                                <td className="px-6 py-4 text-right"> 
-                                    <button onClick={() => handleDelete(event.eventID)} className="font-medium text-red-800 dark:text-red-500 hover:underline">Remove</button>
-                                </td>
-                                <td className="px-6 py-4 text-right"> 
-                                    <button onClick={() => handleShare(event.eventID)} className="font-medium text-green-800 dark:text-green-500 hover:underline"> Share </button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+          {/* Add Event button */}
+          <Link to="/manager?tab=add-event" className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 ml-2 rounded px-4">Add Event</Link>
         </div>
-    );
+      </div>
+
+      <div className="relative overflow-x-auto bg-gray-200 shadow-lg">
+        <Table hoverable className='bg-gray-200 dark:bg-gray-700 dark:text-white'>
+          <Table.Head>
+            <Table.HeadCell  className='dark:bg-gray-800'>ID</Table.HeadCell>
+            <Table.HeadCell  className='dark:bg-gray-800'>Name</Table.HeadCell>
+            <Table.HeadCell  className='dark:bg-gray-800'>Date</Table.HeadCell>
+            <Table.HeadCell  className='dark:bg-gray-800'>Starting Time</Table.HeadCell>
+            <Table.HeadCell  className='dark:bg-gray-800'>Duration (h)</Table.HeadCell>
+            <Table.HeadCell  className='dark:bg-gray-800'>Budget (Rs.)</Table.HeadCell>
+            <Table.HeadCell  className='dark:bg-gray-800'>Ticket Price (Rs.)</Table.HeadCell>
+            <Table.HeadCell  className='dark:bg-gray-800'>Quantity</Table.HeadCell>
+            <Table.HeadCell  className='dark:bg-gray-800'>Entertainer</Table.HeadCell>
+            <Table.HeadCell  className='dark:bg-gray-800'>Description</Table.HeadCell>
+            <Table.HeadCell  className='dark:bg-gray-800 text-center' colSpan={3} >Actions</Table.HeadCell>
+          </Table.Head>
+          <Table.Body>
+            {events
+              .filter(event => {
+                // Filter events based on the search query
+                if (!searchQuery) {
+                  return true; // Return all events if there's no search query
+                } else {
+                  // Check if any of the event properties contain the search query
+                  return Object.values(event).some(value =>
+                    value && value.toString().toLowerCase().includes(searchQuery.toLowerCase())
+                  );
+                }
+              })
+              .map((event, index) => (
+                <Table.Row key={event.eventID} >
+                  <Table.Cell className='text-black dark:text-slate-200'>{event.eventID}</Table.Cell>
+                  <Table.Cell className='text-black dark:text-slate-200'>{event.eventName}</Table.Cell>
+                  <Table.Cell className='text-black dark:text-slate-200'>{event.eventDate}</Table.Cell>
+                  <Table.Cell className='text-black dark:text-slate-200'>{event.startTime}</Table.Cell>
+                  <Table.Cell className='text-black dark:text-slate-200'>{event.duration}</Table.Cell>
+                  <Table.Cell className='text-black dark:text-slate-200'>{event.budget}</Table.Cell>
+                  <Table.Cell className='text-black dark:text-slate-200'>{event.ticketPrice}</Table.Cell>
+                  <Table.Cell className='text-black dark:text-slate-200'>{event.ticketQuantity}</Table.Cell>
+                  <Table.Cell className='text-black dark:text-slate-200'>{event.entertainer}</Table.Cell>
+                  <Table.Cell className='text-black dark:text-slate-200'>{event.description}</Table.Cell>
+                  <Table.Cell>
+                    <button onClick={() => handleUpdate(event.eventID)} className="font-medium text-blue-600 dark:text-blue-500 hover:scale-110">Update</button>
+                  </Table.Cell>
+                  <Table.Cell> 
+                    <button onClick={() => handleDelete(event.eventID)} className="font-medium text-red-800 dark:text-red-500 hover:scale-110">Remove</button>
+                  </Table.Cell>
+                  <Table.Cell> 
+                    <button onClick={() => handleShare(event.eventID)} className="font-medium text-green-800 dark:text-green-500 hover:scale-110">Share</button>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+          </Table.Body>
+        </Table>
+      </div>
+    </div>
+  );
+
 };
 
 export default ViewAllEvents;
