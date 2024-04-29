ec60db1fa4cfe4e805b5482adbd030cea8ef8a7b
Thisara Bandara <thisarabandara2@gmail.com>
2024-04-30 01:19:37 +0530

    Update TableManage.jsx

--- ReactFrontEnd/kingsman/src/pages/manager/advanceSettings/TableManage.jsx
+++ ReactFrontEnd/kingsman/src/pages/manager/advanceSettings/TableManage.jsx
@@ -68,6 +68,7 @@ export default function TableManage() {
 
                 ))}
             </div>
+            {/* Delete table popup  */}
             {deleteTableModel &&
                 <div>
                     <Modal show={deleteTableModel}size="md" onClose={() => setDeleteTableModel(false)} popup>
@@ -76,7 +77,7 @@ export default function TableManage() {
                             <div className="text-center">
                                 <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
                                 <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                                    Are you sure you want to delete this Table?
+                                    Are you sure you want to delete this Table ?
                                 </h3>
                                 <div className="flex justify-center gap-4">
                                     <Button color="failure" onClick={handleDeleteTable}>
