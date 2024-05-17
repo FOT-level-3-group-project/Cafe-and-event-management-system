59dcc6d0dcce3b7b9e29e279c3831ba9078464ad
Thisara Bandara <thisarabandara2@gmail.com>
2024-05-18 02:26:14 +0530

    Create backend function for Notification

---
+++ Spring Boot - Back End/src/main/java/com/kingsman/Kingsman/repository/NotificationRepository.java
@@ -0,0 +1,12 @@
+package com.kingsman.Kingsman.repository;
+
+import com.kingsman.Kingsman.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByForWho(String forWho);
+}
