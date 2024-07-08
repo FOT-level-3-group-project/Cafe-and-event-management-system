180473efc78fb0d92c131e3e7afe568767bb54d1
Chathumina Dilshan <chathuminadilshan14@gmail.com>
2024-07-09 04:46:28 +0530

    Waiter dashboard is Created
    Create Dashboard

--- ReactFrontEnd/kingsman/src/components/WaiterSideBar.jsx
+++ ReactFrontEnd/kingsman/src/components/WaiterSideBar.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Sidebar } from "flowbite-react";
 import { HiArrowSmRight} from "react-icons/hi";
+import { FaChartPie } from "react-icons/fa";
 import { MdRestaurantMenu } from "react-icons/md";
 import { BiSolidFoodMenu } from "react-icons/bi";
 import { logOutSuccess } from '../redux/user/userSlice';
@@ -36,6 +37,11 @@ export default function WaiterSideBar() {
         <Sidebar className='w-full h-full md:w-56'>
             <Sidebar.Items>
                 <Sidebar.ItemGroup>
+                    <Link to='/waiter?tab=dashboard'>
+                        <Sidebar.Item active={tab === 'dashboard'} icon={FaChartPie} as='div' >
+                            Dashboard
+                        </Sidebar.Item>
+                    </Link>
                     <Link to='/waiter?tab=take-order'>
                         <Sidebar.Item active={tab === 'take-order'} icon={MdRestaurantMenu} as='div' >
                             Take Orders
