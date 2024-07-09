99c830b35c3e5ef058975f1817152e481b8068a6
Chathumina Dilshan <chathuminadilshan14@gmail.com>
2024-07-09 06:11:41 +0530

    Create API to get Available Tables

--- Spring Boot - Back End/src/main/java/com/kingsman/Kingsman/repository/TableManageRepository.java
+++ Spring Boot - Back End/src/main/java/com/kingsman/Kingsman/repository/TableManageRepository.java
@@ -4,8 +4,13 @@ import com.kingsman.Kingsman.model.TableManage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TableManageRepository extends JpaRepository<TableManage,Long> {
 
     boolean existsByTableNumber(int tableNumber);
+
+    // Method to find tables based on availability
+    List<TableManage> findByTableAvailability(boolean availability);
 }
